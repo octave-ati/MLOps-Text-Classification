8c695eec277577e0dc9e17fed14c6ce5c615ec29
Octave <fatskill@hotmail.fr>
2023-01-13 12:41:45 +0100

    Documented predict.py

--- classifyops/predict.py
+++ classifyops/predict.py
@@ -1,16 +1,40 @@
 import numpy as np
 
 # Custom predict function
-def custom_predict(y_prob, threshold, index):
-  """Custom predict function that defaults
-  to an index if conditions are not met."""
-  y_pred = [np.argmax(p) if max(p) > threshold[str(np.argmax(p))] else index for p in
-  y_prob]
-  return np.array(y_pred)
+def custom_predict(y_prob, threshold: dict, index: int) -> np.array:
+    """Returns y_prob if the probability is higher than the class threshold.
+
+    Args:
+        y_prob (np.array): Prediction probabilities
+        threshold (dict): Class thresholds. If the probability is above the threshold,
+        The class will be predicted, otherwise the index argument will be return.
+        index (int): Index of the "other" class.
+
+    Returns:
+        np.array: Custom predictions
+    """
+    y_pred = [np.argmax(p) if max(p) > threshold[str(np.argmax(p))] else index for p in
+    y_prob]
+    return np.array(y_pred)
 
 
-def predict(texts, artifacts):
-    """Predict tags for given texts."""
+def predict(texts: List[str], artifacts) -> list[dict]:
+    """Predict tags from a list of strings with provided artifacts
+
+    Args:
+        texts (List[str]): List of utterances
+        artifacts (dict): Dictionary containing the following elements:
+            - vectorizer(model) : Character vectorizer
+            - model (SGDclassifier model) : Trained prediction model
+            - args (dict) : model arguments
+            - label_encoder (LabelEncoder()) : Custom Label Encoder instance.
+            - performance (dict, Optional) : Performance of the provided model.
+
+    Returns:
+        list[dict]: List of dictionaries containing:
+            - input_text(str) : Input utterance
+            - predicted_tags(str) : Predicted label(s)
+    """
     x = artifacts["vectorizer"].transform(texts)
     y_pred = custom_predict(
         y_prob=artifacts["model"].predict_proba(x),
