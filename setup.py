3e593974b44db986b5a4041858e2f88759c36334
faskill <fatskill@hotmail.fr>
2023-01-21 11:03:34 +0100

    Added precommit hooks

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ docs_packages = ["mkdocs==1.3.0", "mkdocstrings==0.18.1"]
 
 style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1", "black[jupyter]"]
 
-test_packages = ["pytest==7.2.1", "pytest-cov==4.0.0", "great-expectations==0.15.44"]
+test_packages = ["pytest==7.2.1", "pytest-cov==4.0.0", "great-expectations==0.15.44",
+    "pre-commit==2.21.0"]
 
 setup(
     name="classifyops",
